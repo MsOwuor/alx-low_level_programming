4ec117ed24f352273419fe9cfef9049017689a17
MsOwuor <immaculateadhiambo79@gmail.com>
2023-06-15 04:51:50 -0700

    size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,5 +11,7 @@ int main(void)
 	printf("Size of short: %zu byte(s)\n", sizeof(short));
 	printf("Size of int: %zu byte(s)\n", sizeof(int));
 	printf("Size of long: %zu byte(s)\n", sizeof(long));
+	printf("Size of long long: %zu byte(s)\n", sizeof(long long));
+	printf("Size of float: %zu byte(s)\n", sizeof(float));
 	return (0);
 }
