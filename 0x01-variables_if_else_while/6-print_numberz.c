62eb4f43b4b789ed493341a90df1dd4f49579763
MsOwuor <immaculateadhiambo79@gmail.com>
2023-06-16 08:26:00 -0700

    umberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print single digits starting from 0
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
