7e551e31c2c81fc1a8c367041b25eafa4d05d0a3
MsOwuor <immaculateadhiambo79@gmail.com>
2023-06-21 04:03:35 -0700

    9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,20 +7,17 @@ void times_table(void)
 {
 	int multiplier;
 	int product;
-
+	int i;
 	for (multiplier = 0; multiplier <= 9; multiplier++)
 	{
-		product = 9 * multiplier;
-		_putchar('9');
-		_putchar(' ');
-		_putchar('x');
-		_putchar(' ');
-		_putchar(multiplier + '0');
-		_putchar(' ');
-		_putchar('=');
-		_putchar(' ');
-		_putchar(product / 10 + '0');
-		_putchar(product % 10 + '0');
+		for (i = 0; i <= 9; i++)
+		{
+			product = i * multiplier;
+			_putchar(product / 10 + '0');
+			_putchar(product % 10 + '0');
+			_putchar(',');
+			_putchar(' ');
+		}
 		_putchar('\n');
 	}
 }
