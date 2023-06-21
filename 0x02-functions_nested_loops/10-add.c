4bf3a09d0cae773e0e4d2c1ab82dd211bdf384e9
MsOwuor <immaculateadhiambo79@gmail.com>
2023-06-21 04:24:12 -0700

    sum of intergers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - adds two integers and returns the result
+ * @a: interger to be added
+ * @b: interger to be added
+ * Return: sum of a and b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
