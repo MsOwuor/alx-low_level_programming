eb604c8e9626c394551e732cc0e55440f4cbd216
MsOwuor <immaculateadhiambo79@gmail.com>
2023-06-22 16:56:22 +0000

    print remaining days

--- 0x03-debugging/3-main_b.c
+++ 0x03-debugging/3-main_b.c
@@ -2,10 +2,10 @@
 #include "main.h"
 
 /**
- * * main - takes a date and prints how many days are left in the year, taking
- * * leap years into account
- * * Return: 0
- * */
+ * main - takes a date and prints how many days are left in the year, taking
+ * leap years into account
+ * Return: 0
+ */
 
 int main(void)
 {
@@ -15,7 +15,7 @@ int main(void)
 
 	month = 2;
 	day = 29;
-        year = 2000;
+	year = 2000;
 
 	printf("Date: %02d/%02d/%04d\n", month, day, year);
 
