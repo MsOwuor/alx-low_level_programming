2314428f2603972af8dcb8ce53451a133d2cc905
MsOwuor <immaculateadhiambo79@gmail.com>
2023-06-22 14:34:21 +0300

    infinite loop

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -2,7 +2,7 @@
 
 /**
  * largest_number - returns the largest of 3 numbers
- * @: first integer
+ * @a: first integer
  * @b: second integer
  * @c: third integer
  * Return: largest number
@@ -20,7 +20,7 @@ int largest_number(int a, int b, int c)
 	{
 		largest = b;
 	}
-	else 
+	else
 	{
 		largest = c;
 	}
