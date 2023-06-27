7d974e8711b3c531f084a6725d3c1d59c85c394f
MsOwuor <immaculateadhiambo79@gmail.com>
2023-06-27 11:18:20 -0700

    crack me

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,29 +1,35 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
-#define password_length 6
 
-char generate_random_char()
+/**
+ * main - program that generates random valid
+ * passwords for the program 101-crackme
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-	return ! + rand() % 94;
-}
-void generate_random_password(char *password)
-{
-	for (int i = 0; i < password_length; i++)
+	int pass[100];
+	int i, sum, n;
+
+	sum = 0;
+
+	srand(time(NULL));
+
+	for (i = 0; i < 100; i++)
 	{
-		password[i] = generate_random_char();
+		pass[i] = rand() % 78;
+		sum += (pass[i] + '0');
+		putchar(pass[i] + '0');
+		if ((2772 - sum) - '0' < 78)
+		{
+			n = 2772 - sum - '0';
+			sum += n;
+			putchar(n + '0');
+			break;
+		}
 	}
-	password[password_length] = '\0';
-}
-int main()
-{
-	srand(time(0));
-
-	char password[password_length + 1];
-	
-	generate_random_password(password);
-
-	printf("Generated password: %s\n", password);
 
 	return (0);
 }
