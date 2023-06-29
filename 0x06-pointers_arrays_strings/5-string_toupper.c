c8d27d77c2aace19808d398610202e60345374d1
MsOwuor <immaculateadhiambo79@gmail.com>
2023-06-29 03:59:21 -0700

    lowercase to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase
+ * @a: string in lowercase
+ * Return: uppercase
+ */
+char *string_toupper(char *a)
+{
+	int i;
+
+	i = 0;
+	while (a[i] != '\0')
+	{
+		if (a[i] >= 'a' && a[i] <= 'z')
+			a[i] = a[i] - 32;
+		i++;
+	}
+	return (a);
+}
+/* 32 because its the difference in lowercase and uppercase ascii value */
