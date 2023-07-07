6458aa74928147150625f94c2b5db07b828dde3d
MsOwuor <immaculateadhiambo79@gmail.com>
2023-07-07 13:35:50 -0700

    multiply

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print product of two arguments
+ * _atoi - converts to integer
+ * @s: string to be converted
+ *@argc: argument counter
+ @argv: numbers to multiply
+ * Return: 0 on success, 1 if not two arguments
+ */
+int main(int argc, char *argv[])
+{
+	int num1, num2, result;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return(1);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	result = num1 * num2;
+
+	printf("%d\n", result);
+
+	return(0);
+}
