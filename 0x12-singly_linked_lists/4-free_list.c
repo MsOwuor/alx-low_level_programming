4f49cc8ae7aac64a340efd3c2fcbdbfce5acceae
MsOwuor <immaculateadhiambo79@gmail.com>
2023-07-27 13:50:59 -0700

    free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include <stddef.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - grees a list_t list
+ * @head: pointer to the head of the list
+ */
+
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
+}
