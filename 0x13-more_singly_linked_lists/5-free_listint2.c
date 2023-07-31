128b4118a90695d772a4b3a14702b5ced25578a6
MsOwuor <immaculateadhiambo79@gmail.com>
2023-07-31 14:59:13 -0700

    free list 2

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,10 +10,14 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
+	if (head == NULL)
+		return;
+
 	while (*head != NULL)
 	{
-		temp = *head;
-		*head = (*head)->next;
-		free(temp);
+		temp = (*head)->next;
+		*head = temp;
+		free(*head);
 	}
+	*head = NULL;
 }
